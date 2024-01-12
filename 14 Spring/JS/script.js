001ce69b0a22717d0e5572168f1d01c975e53721
nyebe-14 <ramirezian037@gmail.com>
2024-01-13 06:16:57 +0800

    Para plex Spring

---
+++ 14 Spring/JS/script.js
@@ -0,0 +1,11 @@
+const layers = document.querySelectorAll('#layer');
+
+document.addEventListener('mousemove', function (e) {
+	layers.forEach((layer, i) => {
+		const movement = layer.getAttribute('data-speed');
+		let x = (window.innerWidth - e.pageX * movement) / 250;
+		let y = (window.innerHeight - e.pageY * movement) / 250;
+
+		layer.style.transform = `translateX(${-x}px) translateY(${-y}px)`;
+	});
+});
